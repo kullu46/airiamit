c40e4e18a236c7d24c139818794a780c6fb4952d
pk0769 <parveenbhati92@gmail.com>
2020-04-28 09:28:46 +0530

    fixed ui issue

--- wp-content/themes/airiamit/custom-functions.php
+++ wp-content/themes/airiamit/custom-functions.php
@@ -1,5 +1,5 @@
 <?php 
-
+error_reporting(0); 
 
 // register new_homes post type
 // Register Custom Post Type
--- wp-content/themes/airiamit/templates/template-home.php
+++ wp-content/themes/airiamit/templates/template-home.php
@@ -17,7 +17,30 @@ get_header();
 			the_post();
             get_template_part( 'template-parts/content-home' );
             ?>
+            
                 <div class="container">
+                <div class="top-content-slider">
+                    <div class="owl-carousel owl-theme top-slider-home">
+                        <?php if( have_rows('home_top_slider') ): ?>
+                            <?php while( have_rows('home_top_slider') ): the_row(); 
+                                $image = get_sub_field('section_icon');
+                                $content = get_sub_field('section_title');
+                                $link = get_sub_field('page_link');
+                                ?>
+                                <div class="item">
+                                    <?php if( $link ): ?>
+                                        <a href="<?php echo $link['url']; ?>">
+                                    <?php endif; ?>
+                                        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
+                                        <?php echo $content; ?>
+                                    <?php if( $link ): ?>
+                                        </a>
+                                    <?php endif; ?>
+                                </div>
+                            <?php endwhile; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
                     <div class="alignfull no-padding no-margin">
                         <?php the_content(); ?>
                     </div>
@@ -34,3 +57,23 @@ get_header();
 <?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
 
 <?php get_footer(); ?>
+
+<script type="text/javascript">
+        jQuery(document).ready(function($){
+            $('.owl-carousel.top-slider-home').owlCarousel({
+                center:true,
+                loop:true,
+                margin:20,
+                dots: true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    992:{
+                        items:3
+                    }
+                }
+            })
+        });
+</script>
+
