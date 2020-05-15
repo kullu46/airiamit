3649cbc8fde595c9422ed3528e090fbc1b0199b6
pk0769 <parveenbhati92@gmail.com>
2020-05-15 21:14:59 +0530

    fix ui issus

--- wp-content/themes/airiamit/header-single-custom_listings.php
+++ wp-content/themes/airiamit/header-single-custom_listings.php
@@ -29,7 +29,7 @@
 		?>
 
 		<header id="site-header" class="header-footer-group single-home-header" role="banner">
-
+		<div class="header-wrap">
 			<div class="container header-inner section-inner">
 
 				<div class="header-titles-wrapper">
@@ -44,6 +44,7 @@
 
 				</div>
 
+			</div>
 			</div>
 
 		</header>
--- wp-content/themes/airiamit/header-single-new_homes.php
+++ wp-content/themes/airiamit/header-single-new_homes.php
@@ -29,6 +29,7 @@
 		?>
 
 		<header id="site-header" class="header-footer-group single-home-header" role="banner">
+		<div class="header-wrap">
 
 			<div class="container header-inner section-inner">
 
@@ -45,7 +46,7 @@
 				</div>
 
 			</div>
-
+			</div>
 		</header>
 
 		<?php
